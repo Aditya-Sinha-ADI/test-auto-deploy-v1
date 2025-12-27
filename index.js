3c7c589d654eba26ad96276a9288bc0e708cb368
Aditya Sinha <adityasinha6060@gmail.com>
2025-12-27 15:07:16 +0530

    test: trigger v2 webhook

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-  res.send("Deployed successfully 🚀");
+  res.send("Deployed successfully 🚀. lets goooooo");
 });
 
 app.listen(PORT, () => {
